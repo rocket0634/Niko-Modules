2829127d6b1eb25ddbc147ff23ed5cfa842bf3bf
River Buizel <4911928+rocket0634@users.noreply.github.com>
2023-05-05 01:50:56 -0700

    Move Version Helper into KMModSource

--- KitchenSinkMaster/Assets/Scripts/KMModSource.cs
+++ KitchenSinkMaster/Assets/Scripts/KMModSource.cs
@@ -1,14 +1,20 @@
+using Assets.Scripts.Mods;
+using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 /// <summary>
 /// Use this component to gain access to your project's mod identification (modID) from within Keep Talking and Nobody Explodes.
 /// </summary>
 /// <remarks>
 /// <para>Mods that share the same ModID are not supported, however, the game does not have any protections against doing so. Be careful to make sure the ModID of your mod doesn't conflict with another while developing your mod.</para>
-/// <para>This component is required to be attached to a gameobject in order to function properly.</para>
+/// <para>This component is recommended to be attached to a prefab tagged as mod.bundle in order to function properly.</para>
 /// </remarks>
 public class KMModSource : MonoBehaviour
 {
     private static string _modID;
+    private static IEnumerable<Mod> _mods;
+    private static string _modPath;
+    private static string _version;
     /// <returns>
     /// The identifer for your mod. If the ModSource component is not available or if this is called in the Editor, it will instead return the calling assembly's name.
     /// </returns>
@@ -19,6 +25,26 @@ public class KMModSource : MonoBehaviour
         } 
     }
 
+    /// <returns>
+    /// Obtains the path used to load this mod. This can only be guaranteed if this component is attached to a prefab that is not instantiated. Otherwise, it will return null.
+    /// </returns>
+    internal string ModPath
+    {
+        get
+        {
+            return _modPath ?? GetModPath();
+        }
+    }
+    internal string Version()
+    {
+        if (Application.isEditor)
+        {
+            Debug.Log("check \"Configure Mod\"");
+            return "NA";
+        }
+        return _version ?? (_version = GetVersion());
+    }
+
     private string GetModID()
     {
         ModSource source = GetComponent<ModSource>();
@@ -27,5 +53,39 @@ public class KMModSource : MonoBehaviour
         if (source != null && !string.IsNullOrEmpty(source.ModName))
             _modID = source.ModName;
         return _modID;
+    }
+
+    private string GetModPath()
+    {
+        var applicableDirs = ModManager.Instance.loadedMods.Where(x => x.Value.ModID == ModID);
+        _mods = applicableDirs.Count() > 0 ? applicableDirs.Select(x => x.Value) : new List<Mod>();
+        var dir = _mods.FirstOrDefault(x => x.ModObjects != null && x.ModObjects.Contains(gameObject));
+        _modPath = dir == default(Mod) ? null : dir.modDirectory;
+        return _modPath;
+    }
+
+    private string GetVersion()
+    {
+        // This only works if KMModSource is directly referenced from a prefab with mod.bundle
+        // If the gameobject is instantiated, obviously the game object won't be the same.
+        // Using this can guarantee which mod we loaded in from, without any guessing.
+        var dir = ModPath ?? _mods.First().modDirectory;
+        var applicableInfos = _mods.ToDictionary(x => x.modDirectory, x =>
+        {
+            ModInfo y;
+            // InstalledModInfos isn't written to by Tweaks, and won't work if workshop mods are loaded locally.
+            if (!ModManager.Instance.InstalledModInfos.TryGetValue(x.modDirectory, out y))
+                ModManager.Instance.GetModInfoFromPath(x.modDirectory, 0);
+            return y;
+        });
+        if (applicableInfos.Count < 1)
+            return "NA";
+        else if (applicableInfos.Count() > 1)
+        {
+            Debug.LogFormat("[KMModSource] There is more than one mod installed on this machine with the modID \"{0}\"]: \n"
+            + "{1}", _modID, applicableInfos.Select(x => x.Key + ": Version " + x.Value.Version).Join("\n"));
+        }
+        var info = applicableInfos[dir];
+        return _version = info.Version ?? "NA";
     }
 }
--- KitchenSinkMaster/Assets/Sink Base/SinkScaffold.cs
+++ KitchenSinkMaster/Assets/Sink Base/SinkScaffold.cs
@@ -4,9 +4,9 @@ using UnityEngine;
 
 public class SinkScaffold : MonoBehaviour {
 	#pragma warning disable 649 // Stop compiler warning: "Field is not assigned to" (they are assigned in Unity)
-    public KMModSource ModSource;
 	public KMSelectable Cold, Hot;
 	public Texture[] KnobColors;
+    public KMModSource ModSource;
     
     #region Faulty
     public KMSelectable Faucet, Pipe, Basin;
