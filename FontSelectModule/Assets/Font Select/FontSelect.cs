rocket0634/Niko-Modules

commit acf524788268026b812c6c2de1443a30b22c591f

Change the font disable option to a bool

FontSelectModule/Assets/Font Select/FontSelect.cs
@@ -70,8 +70,9 @@ public class FontSelect : MonoBehaviour
     {
         ModConfig modConfig = new ModConfig("FontSettings", typeof(FontSettings));
         Settings = (FontSettings)modConfig.Settings;
+        modConfig.Settings = Settings;
         FontSelect_moduleId = FontSelect_moduleIdCounter++;
-        banned = Settings.disableKarmaMerriweather == "1";
+        banned = Settings.disableKarmaMerriweather;
         int[] ListNumbers = { 1, 4, 4, 2, 3, 3, 1, 2, 3, 1, 3, 4, 4, 1, 2, 2 };
         string[] ListPhrases = { "Eight Ate 8", "Jokes on you!\nI'm the male.", "Jokes on you!\nI'm male.", "Testing,\ntesting,\n1 to 3.", "Yew R. Wonn", "Jokes on you!\nI'm the mail.", "Ewe Arr Won", "888", "U.R. 1", "You are one", "Ate, Ate, Ate", "8 ate eight", "Testing,\ntesting,\n123", "Testing,\ntesting,\n1-3", "Jokes on you!\nI'm female.", "Testing,\ntesting,\n1 two 3." };
 
@@ -223,6 +224,22 @@ public class FontSelect : MonoBehaviour
         }
     }
 
+    static Dictionary<string, object>[] TweaksEditorSettings = new Dictionary<string, object>[]
+    {
+        new Dictionary<string, object>
+        {
+            { "Filename", "FontSettings.json" },
+            { "Name", "Font Select" },
+            { "Listing", new List<Dictionary<string,object>> {
+                new Dictionary<string, object>
+                {
+                    { "Key", "disableKarmaMerriweather" },
+                    { "Text", "Disable Karma and Merriweather" },
+                }
+            } }
+        }
+    };
+
     private void DebugLog(string log, params object[] args)
     {
         var logData = string.Format(log, args);
@@ -231,7 +248,7 @@ public class FontSelect : MonoBehaviour
 }
 class FontSettings
 {
-    public string disableKarmaMerriweather = "0";
+    public bool disableKarmaMerriweather = false;
 }
 
 class ModConfig

commit 18299de008617b3d2f6c970b1b69dd68f9e1c383

fix tweaks integration
Addresses a bug introduced in acf5247 that was migrated to several other modules (belonging to blan)

Also proper versioning I guess.

FontSelectModule/Assets/Editor/Resources/ModConfig.asset
@@ -13,8 +13,8 @@ MonoBehaviour:
   m_EditorClassIdentifier: 
   id: FontSelect
   title: Font Select
-  description: "Module by JoketteWuzHere\n\nModule ID: FontSelect\n\r\nManual (HTML):
-    https://ktane.timwi.de/HTML/Font%20Select.html"
-  version: 1.0
+  author: 
+  description: "Module by McNiko67\n\nModule ID: FontSelect\n\r\nManual (HTML): https://ktane.timwi.de/HTML/Font%20Select.html"
+  version: 2.1.4
   outputFolder: build
   previewImage: {fileID: 2800000, guid: 2090177e0e043ca4286b11e9813be37a, type: 3}

FontSelectModule/Assets/Font Select/FontSelect.cs
@@ -32,6 +32,7 @@ public class FontSelect : MonoBehaviour
     protected int CurrentFont = 1;
     protected bool FontSelected;
     private int[] FontList = { 0, 1, 2, 3, 4, 5, 6, 7, 8 };
+    private static bool versionShowed;
 
     public string TwitchHelpMessage = "Use !{0} Left or !{0} Right to cycle fonts, !{0} Submit to submit the current font.";
 
@@ -70,6 +71,13 @@ public class FontSelect : MonoBehaviour
 
     protected void Start()
     {
+        
+        if (!versionShowed)
+        {
+            Debug.LogFormat("<Font Select> Mod version 2.1.4");
+            versionShowed = true;
+        }
+
         ModConfig<FontSettings> modConfig = new ModConfig<FontSettings>("FontSettings");
         Settings = modConfig.Settings;
         modConfig.Settings = Settings;
@@ -232,11 +240,12 @@ public class FontSelect : MonoBehaviour
         {
             { "Filename", "FontSettings.json" },
             { "Name", "Font Select" },
-            { "Listing", new List<Dictionary<string,object>> {
+            { "Listings", new List<Dictionary<string,object>> {
                 new Dictionary<string, object>
                 {
                     { "Key", "disableKarmaMerriweather" },
                     { "Text", "Disable Karma and Merriweather" },
+                    { "Description", "Disables the two fonts that look similar to one another." }
                 }
             } }
         }
@@ -261,6 +270,11 @@ public class FontSelect : MonoBehaviour
             yield return true;
         }
         yield return Submit.OnInteract();
+    }
+
+    private void OnDestroy()
+    {
+        versionShowed = false;
     }
 }
 class FontSettings